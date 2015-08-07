520889593d73ad87dba5bde562f6bc29126e0701
Raghav Sai Cheedalla <raghavsaicheedalla@Raghavs-MacBook-Pro.local>
2015-08-06 19:15:36 -0700

    Initial Commit

---
+++ SlidingMenu/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SlidingMenu
+//
+//  Created by Raghav Sai Cheedalla on 8/6/15.
+//  Copyright (c) 2015 Raghav Sai Cheedalla. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
